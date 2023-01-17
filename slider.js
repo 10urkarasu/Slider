46a9bd5279005bd399b611df6422213b18ebff29
Onur Karasu <onur.karasu@flokserkimya.com.tr>
2023-01-17 16:51:56 +0300

    first commit

---
+++ slider.js
@@ -0,0 +1,8 @@
+const left =document.querySelector(".left")
+const right =document.querySelector(".right")
+const slider =document.querySelector(".slider")
+let slideNumber=1;
+right.addEventListener("click",()=>{
+    slider.style.transform=`translateX(-${slideNumber*800}px)`
+    console.log("tıklandı");
+});
